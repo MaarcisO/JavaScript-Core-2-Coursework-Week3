bb25ac6a8f5007e682a826b492990cd504e69d78
Maarcis <markbodonnell@gmail.com>
2021-09-28 22:01:20 +0100

    finished Alarm Clock

--- mandatory/1-alarmclock/alarmclock.js
+++ mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,32 @@
-function setAlarm() {}
+
+let setAlarmBtn = document.getElementById("set");
+let stopBtn = document.getElementById("stop");
+let timeRemainingText = document.getElementById("timeRemaining");
+
+let intervalID;
+
+function setAlarm() {
+  let setTimeValue = document.getElementById("alarmSet").value;
+  let count=setTimeValue;
+  
+  setAlarmBtn.addEventListener("click", function(){
+    intervalID = setInterval(function() {
+      
+      count -= 1;
+      
+      if(count == 0){
+        pauseAlarm();
+        playAlarm();
+        console.log("alarm!");
+        clearInterval(intervalID); 
+    }
+      
+      timeRemainingText.textContent = "Time Remaining: " + count;
+    }, 1000);    
+  })
+}
+
+
 
 // DO NOT EDIT BELOW HERE
 
